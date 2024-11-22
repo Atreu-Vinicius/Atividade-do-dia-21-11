33d5b0bdb1279399e35ae66cfe2bd3c0cd3798cd
Atreu <atreu51535066@edu.pe.senac.br>
2024-11-22 19:31:19 -0300

    Atividade do dia 21/11/2024

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar dark color="primary">
+ <div className="container">
+ <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+ <div>Aluno: Atreu Vinicius Isidio de Oliveira Miranda </div>
+ </div>
+ </Navbar>
+    </div>
+  );
+}
+
+export default App;
